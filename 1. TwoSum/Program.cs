542e2e9eeb3dc7c438d5df353be8e640a63923a7
Yuu <yy556023w@gmail.com>
2023-08-24 16:26:42 +0800

    solve 1. TwoSum

--- 1. TwoSum/Program.cs
+++ 1. TwoSum/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] nums = new int[] { 2, 7, 11, 15 };
+        int target = 9;
+        var result = Solution(nums, target);
+        Console.WriteLine();
+    }
+
+    static int[] Solution(int[] nums, int target)
+    {
+        int i;
+        int j = 0;
+
+        for (i = 0; i < nums.Length; i++)
+        {
+            for (j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+            }
+        }
+
+        return Array.Empty<int>();
     }
 }
