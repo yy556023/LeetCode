a61ab46d6aa899d039e796acf10f2b20c2178a6d
Yuu <yy556023@gmail.com>
2023-09-03 03:55:08 +0800

    add 9. Palindrome Number

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _9._Palindrome_Number;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution(121);
+    }
+
+    static bool Solution(int x)
+    {
+        int r = 0, c = x;
+        while (c > 0)
+        {
+            r = r * 10 + c % 10;
+            c /= 10;
+        }
+        return r == x;
+    }
+}
